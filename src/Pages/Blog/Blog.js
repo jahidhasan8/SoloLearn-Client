6476c4e707507af24f0d4a8efbfe46f464874a7d
jahidhasan8 <jahidhasan1932@gmail.com>
2022-10-25 00:56:21 +0600

    folder structure and routes created

---
+++ src/Pages/Blog/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            <h4>this is blog component</h4>
+        </div>
+    );
+};
+
+export default Blog;
---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,40 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main";
+import Blog from "../../Pages/Blog/Blog";
+import Courses from "../../Pages/Courses/Courses";
+import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Register from "../../Pages/Register/Register";
+
+
+export const routes=createBrowserRouter([
+    {
+        path:'/',
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/courses',
+                element:<Courses></Courses>
+            },
+            {
+                path:'/blog',
+                element:<Blog></Blog>
+            },
+            {
+                
+            },
+            {
+                path:'/login',
+                element:<Login></Login>
+            },
+            {
+                path:'/register',
+                element:<Register></Register>
+            }
+        ]
+    }
+])
