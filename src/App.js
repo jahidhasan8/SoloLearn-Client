59e6d1057f9c0d1b37403464b60811555bc79003
jahidhasan8 <jahidhasan1932@gmail.com>
2022-10-25 01:15:29 +0600

    navbar, favicon, logo added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './Routes/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
       
+
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { SiSololearn } from "react-icons/si";
+import { Link } from 'react-router-dom';
+
+
 
 const Header = () => {
     return (
-        <div>
-            <h2>header</h2>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to={"/"}> <SiSololearn/> SoloLearn</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+                        <Nav.Link as={Link} to={"/courses"}>Courses</Nav.Link>
+                        <Nav.Link as={Link} to={"/blog"}>Blog</Nav.Link>
+                        
+                    </Nav>
+                    <Nav>
+                        <Nav.Link as={Link} to={"/login"}>Login</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
