cb82e65b289a3d9bc1a3abbb8d0a48e7bd6bc5f6
jahidhasan8 <jahidhasan1932@gmail.com>
2022-10-24 23:37:18 +0600

    firebase config file added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLTokqa0B5pd_Y34OytcJ3PZwdA0qpgJI",
+  authDomain: "sololearn-ccd33.firebaseapp.com",
+  projectId: "sololearn-ccd33",
+  storageBucket: "sololearn-ccd33.appspot.com",
+  messagingSenderId: "310153979294",
+  appId: "1:310153979294:web:dece7aed1d70fb8c23628f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
